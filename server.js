558978f9154961e98708a16ff7c8e16e1ab40e49
Kayli Garwood <kgarwood@uncc.edu>
2020-09-29 12:51:43 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,31 +1,16 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-]
-};
-
 app.get('/hello', (req, res) => {
     res.send('Hello Word!');
 });
 
 app.get('/budget', (req, res) => {
+    var budget = require("./budget.json");
     res.json(budget);
 });
 
