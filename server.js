3951de8814cc8470041ca86efa7b94c65bdebe49
Kayli Garwood <kgarwood@uncc.edu>
2020-10-04 01:24:19 -0400

    Implementing CORS

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const { response } = require('express');
+//Budget API
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello Word!');
-});
+const budget = {
+    myBudget: [
+        {
+            title:'Eat out',
+            budget: 25
+        },
+        {
+            title:'Rent',
+            budget: 275
+        },
+        {
+            title:'Grocery',
+            budget: 110
+        },
+    ]
+};
 
 app.get('/budget', (req, res) => {
     var budget = require("./budget.json");
@@ -15,5 +29,5 @@ app.get('/budget', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
